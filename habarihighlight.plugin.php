f2e9de6803cdb482ca93469bf2c2177b50e2ab18
Michael Bishop <miklb@miklb.com>
2014-01-30 02:06:41 -0500

    load the js in the footer

--- habarihighlight.plugin.php
+++ habarihighlight.plugin.php
@@ -4,8 +4,8 @@ class HabariHighlight extends Plugin {
 	public function action_template_header () 
 	{
 		Stack::add('template_stylesheet','http://yandex.st/highlightjs/8.0/styles/solarized_light.min.css','highlight');
-		Stack::add('template_header_javascript','http://yandex.st/highlightjs/8.0/highlight.min.js','highlightjs');
-		Stack::add('template_header_javascript',$this->get_url() . '/habarihighlight.js', 'hljs', 'highlightjs');
+		Stack::add('template_footer_javascript','http://yandex.st/highlightjs/8.0/highlight.min.js','highlightjs');
+		Stack::add('template_footer_javascript',$this->get_url() . '/habarihighlight.js', 'hljs', 'highlightjs');
 	}
 
 }
